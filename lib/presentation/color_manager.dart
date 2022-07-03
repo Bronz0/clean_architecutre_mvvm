707a8c0d1f1afb21456d560dd8acfc0bf42b1bec
unknown <boudissa.mn@gmail.com>
2022-07-03 15:08:38 +0100

    added color manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorManager{
+  static Color primary = HexColor.fromHex("#ED9728");
+  static Color darkGrey = HexColor.fromHex("#525252");
+  static Color grey = HexColor.fromHex("#737477");
+  static Color lightGrey = HexColor.fromHex("#9E9E9E");
+  static Color primaryOpacity70 = HexColor.fromHex("#70ED9728");
+}
+
+
+extension HexColor on Color {
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+}
