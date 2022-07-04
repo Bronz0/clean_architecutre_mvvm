fd57160f8363ef3bb059288583b7765c5eb76df4
unknown <boudissa.mn@gmail.com>
2022-07-04 10:59:15 +0100

    added style manager

---
+++ lib/presentation/style_manager.dart
@@ -0,0 +1,37 @@
+import 'package:clean_architecture_mvvm/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(double fontSize, String fontFamily,FontWeight fontWeight, Color color){
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    color: color
+  );
+}
+
+// return regular style
+
+TextStyle getRegularStyle({double fontSize = FontSize.s12, required Color color}){
+  return _getTextStyle(fontSize, FontConstatns.fontFamily,FontWeightManager.regular, color);
+}
+
+
+// return light style
+TextStyle getLightStyle({double fontSize = FontSize.s12, required Color color}){
+  return _getTextStyle(fontSize, FontConstatns.fontFamily,FontWeightManager.light, color);
+}
+
+// return bold style
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}){
+  return _getTextStyle(fontSize, FontConstatns.fontFamily,FontWeightManager.bold, color);
+}
+
+// return medium style
+TextStyle getMediumStyle({double fontSize = FontSize.s12, required Color color}){
+  return _getTextStyle(fontSize, FontConstatns.fontFamily,FontWeightManager.medium, color);
+}
+
+// return semi bold style
+TextStyle getSemiBoldStyle({double fontSize = FontSize.s12, required Color color}){
+  return _getTextStyle(fontSize, FontConstatns.fontFamily,FontWeightManager.semiBold, color);
+}
